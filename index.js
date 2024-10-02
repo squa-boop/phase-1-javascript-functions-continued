34ea9adc57bcfdbed559b64ddda8773e91480822
blessed wesonga <blessedwesonga@gmail.com>
2024-10-02 19:03:40 +0300

    phase-1-javascript-functions-continued

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // code your solution here
+// 1. Function saturdayFun
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;  
+}
+
+// 2. Function mondayWork
+function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;  
+}
+
+// 3. Function wrapAdjective
+function wrapAdjective(adjective = "*") {
+    return function(parameter = "special") {
+        return `You are ${adjective}${parameter}${adjective}!`;  
+    };
+}
+
+
+
+
+
+
+
+    
+
+
+
+
